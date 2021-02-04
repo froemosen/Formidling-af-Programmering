44eef32057677380dc80ab35d82401557429ea40
froemosen <kristianantonhedegaard@gmail.com>
2021-02-04 10:36:36 +0100

    Yo

--- Menu.py
+++ Menu.py
@@ -14,7 +14,7 @@ pg.display.set_caption("Life of Mark episode 2 - Master The Snake")
 icon = pg.image.load("Assets\others\icon.png")
 pg.display.set_icon(icon)
 select = pg.mixer.Sound("Assets\Sounds\selectMenu.wav")
-btnStartX, btnStartY, btnStartHeight, btnStartWidth, btnQuitX, btnQuitY, btnQuitHeight, btnQuitWidth = 720, 440, 60, 178, 700, 840, 70, 238
+btnStartX, btnStartY, btnStartHeight, btnStartWidth, btnQuitX, btnQuitY, btnQuitHeight, btnQuitWidth = 841, 600, 60, 238, 872, 840, 58, 176
 buttons = Classes.buttons(btnStartX, btnStartY, btnStartHeight, btnStartWidth, btnQuitX, btnQuitY, btnQuitHeight, btnQuitWidth)
 startButton1 = Classes.startButton1
 quitButton1 = Classes.quitButton1
@@ -26,45 +26,40 @@ def drawWorld(baggrundValg):
         buttons.drawStart()
         buttons.drawQuit()
 
-        if mx > 850 and mx < 1070 and my > 520 and my < 560:
-            win.blit(icon, (783, 480))
-            win.blit(startButton1, (720, 440))
+        if mx > btnStartX and mx < btnStartX+btnStartWidth and my > btnStartY and my < btnStartY+btnStartHeight:
+            win.blit(icon, (btnStartX-30, btnStartY))
+            win.blit(startButton1, (btnStartX, btnStartY))
             if pg.mixer.Channel(1).get_busy() == False:
                 pg.mixer.Channel(1).play(select)
                 
-        elif mx > 880 and mx < 1035 and my > 930 and my < 965:
-            win.blit(quitButton1, (700, 840))
+        elif mx > btnQuitX and mx < btnQuitX+btnQuitWidth and my > btnQuitY and my < btnQuitY+btnQuitHeight:
+            win.blit(quitButton1, (btnQuitX, btnQuitY))
             if pg.mixer.Channel(2).get_busy() == False:
                 pg.mixer.Channel(2).play(select)
         
         else:
-            win.blit(bg, (0,0))
             buttons.drawStart()
             buttons.drawQuit()
 
 def pygameMenuStart():
     pg.init()
     pg.mixer.music.load("Assets\Sounds\MainMenuMusic.mp3")
-    pg.mixer.music.set_volume(0.3)
+    pg.mixer.music.set_volume(0.35)
     pg.mixer.music.play(-1)
     baggrundValg = 21
     run = True
     while run:
         mx, my = pg.mouse.get_pos()
         clock.tick(60)
-        baggrundValg = baggrundValg + 1
-
-        if baggrundValg > 61:
-            baggrundValg = r.randint(1, 40)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
             if event.type == pg.MOUSEBUTTONDOWN:
-                if mx > 850 and mx < 1070 and my > 520 and my < 560:
+                if mx > btnStartX and mx < btnStartX+btnStartWidth and my > btnStartY and my < btnStartY+btnStartHeight:
                     pg.mixer.music.fadeout(1500)
                     Game.start()
-                if mx > 880 and mx < 1035 and my > 930 and my < 965:
+                if mx > btnQuitX and mx < btnQuitX+btnQuitWidth and my > btnQuitY and my < btnQuitY+btnQuitHeight:
                     run = False
                     pg.quit()
         drawWorld(baggrundValg)
