abd0c3f73c566c01b995a813859f68f0ae7d3ebe
froemosen <kristianantonhedegaard@gmail.com>
2021-03-05 15:00:24 +0100

    Idk lots of stuffs

--- Game.py
+++ Game.py
@@ -8,7 +8,9 @@ import os
 pg.init()
 pg.font.init()
 win = pg.display.set_mode((1920, 1080), pg.FULLSCREEN)
+
 bg = pg.image.load("Assets\gameSprites\Background.png")
+terminal = pg.image.load("Assets\gameSprites\Terminal.png")
 i = 0
 #level = pg.image.load()
 """for file in os.listdir("dirname") #Bruges til at finde de obstacles som man vil bruge"""
@@ -26,6 +28,7 @@ def run():
 def drawWorld():
     #Graphics and such
     win.blit(bg, (0, 0))
+    win.blit(terminal, (1260, 15))
 
     
 
